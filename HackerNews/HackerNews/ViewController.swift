e2f5fc8c7e50934b5ad18d5c4a9fcbaa686a8665
Alex Ge <alexander.ge@bestseller.com>
2019-08-28 15:55:47 +0100

    create project

---
+++ HackerNews/HackerNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackerNews
+//
+//  Created by Alexander Ge on 28/08/2019.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
