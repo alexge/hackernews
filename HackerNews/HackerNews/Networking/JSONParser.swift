3b4c77384c5d973938a2e9aadb897a0458a70e89
Alex Ge <alexander.ge@bestseller.com>
2019-08-30 17:27:05 +0100

    request performer tests

--- HackerNews/HackerNews/Networking/JSONParser.swift
+++ HackerNews/HackerNews/Networking/JSONParser.swift
@@ -23,27 +23,44 @@ class JSONParser {
     }
     
     private func item(from json: [String:Any]) -> Item? {
-        guard let id = json["id"] as? Int,
+        if let id = json["id"] as? Int,
             let title = json["title"] as? String,
             let user = json["by"] as? String,
             let score = json["score"] as? Int,
-            let type = json["type"] as? String
-            else {
-                return nil
+            let type = json["type"] as? String {
+            
+            let comments = json["kids"] as? [Int] ?? []
+            return Item(id: id, title: title, user: user, score: score, description: type, commentList: comments, comments: [])
+            
+        } else if let stringid = json["id"] as? String,
+            let id = Int(stringid),
+            let title = json["title"] as? String,
+            let user = json["by"] as? String,
+            let stringscore = json["score"] as? String,
+            let score = Int(stringscore),
+            let type = json["type"] as? String {
+            
+            return Item(id: id, title: title, user: user, score: score, description: type, commentList: [], comments: [])
+        } else {
+            return nil
         }
-        let comments = json["kids"] as? [Int] ?? []
-        
-        return Item(id: id, title: title, user: user, score: score, description: type, commentList: comments, comments: [])
     }
     
     private func comment(from json: [String:Any]) -> Comment? {
-        guard let id = json["id"] as? Int,
+        if let id = json["id"] as? Int,
             let parent = json["parent"] as? Int,
-            let text = json["text"] as? String
-            else {
-                return nil
-        }
+            let text = json["text"] as? String {
+            
+            return Comment(id: id, parent: parent, text: text, level: 0)
+        } else if let stringid = json["id"] as? String,
+            let id = Int(stringid),
+            let stringparent = json["parent"] as? String,
+            let parent = Int(stringparent),
+            let text = json["text"] as? String {
         
-        return Comment(id: id, parent: parent, text: text, level: 0)
+            return Comment(id: id, parent: parent, text: text, level: 0)
+        } else {
+            return nil
+        }
     }
 }
--- HackerNews/HackerNewsTests/RequestPerformerTests.swift
+++ HackerNews/HackerNewsTests/RequestPerformerTests.swift
@@ -26,7 +26,105 @@ class RequestPerformerTests: XCTestCase {
                 XCTFail("Array does not match")
             }
         }
-        waitForExpectations(timeout: 3)
+        waitForExpectations(timeout: 1)
     }
 
+    func testFetchItemList_WithoutIntArrayData_ShouldNotCallHandler() {
+        // Arrange
+        let mockURLSession = MockURLSession(objectToReturnAsData: ["hello":"goodbye"])
+        let requestPerformer = RequestPerformer(urlSession: mockURLSession)
+        let failureExpectation = expectation(description: "called handler")
+        failureExpectation.isInverted = true
+        
+        // Act
+        requestPerformer.fetchTopItemIds { array in
+            // Assert
+            failureExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 1) { (error) in
+            XCTAssertTrue(true)
+        }
+    }
+    
+    func testFetchItemDetail_WithItemDictionaryDataResponse_ShouldCallHandler() {
+        // Arrange
+        let itemDict: [String:String] = ["id":"1","title":"thisisatitle","by":"author","score":"1","type":"story"]
+        let mockURLSession = MockURLSession(objectToReturnAsData: itemDict)
+        let requestPerformer = RequestPerformer(urlSession: mockURLSession)
+        let successExpectation = expectation(description: "success response")
+        
+        // Act
+        requestPerformer.fetchItemDetail(itemID: "1") { item in
+            // Assert
+            if item?.id == 1,
+                item?.title == "thisisatitle",
+                item?.user == "author",
+                item?.score == 1,
+                item?.description == "story" {
+                
+                successExpectation.fulfill()
+            } else {
+                XCTFail("Item does not match")
+            }
+        }
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testFetchItemDetail_WithoutItemDictionaryDataResponse_ShouldNotCallHandler() {
+        // Arrange
+        let itemDict: [String:String] = ["id":"1"]
+        let mockURLSession = MockURLSession(objectToReturnAsData: itemDict)
+        let requestPerformer = RequestPerformer(urlSession: mockURLSession)
+        let failureExpectation = expectation(description: "failure response")
+        failureExpectation.isInverted = true
+        
+        // Act
+        requestPerformer.fetchItemDetail(itemID: "1") { item in
+            // Assert
+            failureExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 1) { (_) in
+            XCTAssertTrue(true)
+        }
+    }
+    
+    func testFetchComment_WithCommentDictionaryDataResponse_ShouldCallHandler() {
+        // Arrange
+        let commentDict: [String:String] = ["id":"1","parent":"1","text":"thisisacomment"]
+        let mockURLSession = MockURLSession(objectToReturnAsData: commentDict)
+        let requestPerformer = RequestPerformer(urlSession: mockURLSession)
+        let successExpectation = expectation(description: "success response")
+        
+        // Act
+        requestPerformer.fetchComment(commentID: "1") { comment in
+            // Assert
+            if comment?.id == 1,
+                comment?.text == "thisisacomment",
+                comment?.parent == 1 {
+                
+                successExpectation.fulfill()
+            } else {
+                XCTFail("Item does not match")
+            }
+        }
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testFetchComment_WithoutCommentDictionaryDataResponse_ShouldNotCallHandler() {
+        // Arrange
+        let commentDict: [String:String] = ["id":"1"]
+        let mockURLSession = MockURLSession(objectToReturnAsData: commentDict)
+        let requestPerformer = RequestPerformer(urlSession: mockURLSession)
+        let failureExpectation = expectation(description: "failure response")
+        failureExpectation.isInverted = true
+        
+        // Act
+        requestPerformer.fetchComment(commentID: "1") { comment in
+            // Assert
+            failureExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 1) { (_) in
+            XCTAssertTrue(true)
+        }
+    }
 }
